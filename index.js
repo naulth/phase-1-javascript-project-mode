a3139bdf38a203ea0b100cc74b2c69b8b3770387
Hayden Nault <naulth@gmail.com>
2023-02-13 12:15:46 -0500

    fetch request

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-fetch('https://swapi.dev/api/people')
-    .then(r => r.json)
-    .then((json) => {
-        console.log(json)
+const characterList = document.getElementById('character-list')
+const characterImage = document.getElementById('character-image')
+
+fetch('http://localhost:3000/people')
+    .then(r => r.json())
+    .then(peopleList => { 
+        peopleList.forEach(character => {
+            let characterLi = document.createElement('li')
+            characterLi.textContent = character.name
+            characterList.append(characterLi)
+
+            characterLi.addEventListener('click', e =>{
+                characterImage.src = character.image 
+                characterImage.alt = `${character.name} image`
+            })
+        })
     })
+
+
