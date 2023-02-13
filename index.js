d0b549bbc364af166ee6690fa61a04c99418bae6
Kyle <kyleoneill26@gmail.com>
2023-02-13 12:06:00 -0600

    add toggle

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const characterList = document.getElementById('character-list')
 const characterImage = document.getElementById('character-image')
+const toggleButton = document.getElementById('toggle')
+const darkSide = document.getElementsByClassName('dark')
 
 fetch('http://localhost:3000/people')
     .then(r => r.json())
-    .then(peopleList => { 
+    .then(peopleList => {        
         peopleList.forEach(character => {
             let characterLi = document.createElement('li')
             characterLi.textContent = character.name
@@ -13,7 +15,28 @@ fetch('http://localhost:3000/people')
                 characterImage.src = character.image 
                 characterImage.alt = `${character.name} image`
             })
+            if (character.side === "light"){
+                characterLi.className = "light"
+            } else{
+                characterLi.className = "dark"
+            }
+            
+        })
+        toggleButton.addEventListener('change', e =>{
+            //darkSide.style.display = "none"
+            let lightList = peopleList.filter (character => {
+                if(character.side === "light"){
+                    return true
+                }
+                
+            })
+            console.log(lightList)
         })
     })
+
+
+
+
+
 
 
